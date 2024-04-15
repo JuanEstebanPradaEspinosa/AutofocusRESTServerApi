71fec1bef72718649d28ca9eb95d34cd6fac0f0f
JuanEstebanPradaEspinosa <Juan.pradaespinosa@student.hogent.be>
2024-04-15 13:35:29 +0200

    project live share test

--- Backend/AF.Domain/obj/Debug/net8.0/AF.Domain.AssemblyInfo.cs
+++ Backend/AF.Domain/obj/Debug/net8.0/AF.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AF.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178455dd5617aa38f20b37c078b44d2ef13e4085")]
 [assembly: System.Reflection.AssemblyProductAttribute("AF.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AF.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
